145a6a262b6be98ee88ffddefeb53cbcac65a328
Bart0110 <Bart0110@users.noreply.github.com>
2021-07-10 14:49:02 +0200

    Update scripts

--- update-gh-pages.ps1
+++ update-gh-pages.ps1
@@ -1,10 +1,11 @@
 Remove-Item -LiteralPath "docs" -Force -Recurse
 
 git worktree prune
-git worktree add -B gh-pages docs origin/gh-pages
+git checkout -b gh-pages
 npm run docs
 cd docs
 git add .
 git commit -m "Update docs"
 cd ..
 git push -f origin gh-pages
+git checkout -b master
--- update-gh-pages.sh
+++ update-gh-pages.sh
@@ -2,10 +2,11 @@
 set -e
 rm -rf docs/
 git worktree prune
-git worktree add -B gh-pages docs origin/gh-pages
+git checkout -b gh-pages
 npm run docs
 cd docs
 git add .
 git commit -m "Update docs"
 cd ..
 git push -f origin gh-pages
+git checkout -b master
